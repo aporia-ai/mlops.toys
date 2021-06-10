256da2e847e35c6f0efee54b157a515b0bcbe019
Snir Shechter <snir@aporia.com>
2021-06-10 17:15:02 +0300

    Fixes the meta tags images

--- nuxt.config.js
+++ nuxt.config.js
@@ -145,13 +145,11 @@ export default {
       },
       {
         name: 'og:image',
-        content:
-          'https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png',
+        content: 'https://mlops.toys/images/seo/image.png',
       },
       {
         name: 'twitter:image',
-        content:
-          'https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png',
+        content: 'https://mlops.toys/images/seo/twitter-image.png',
       },
       {
         name: 'twitter:card',
